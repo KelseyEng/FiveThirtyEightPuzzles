cb019e543e3ae7b77974092e82b5b7f2d9a82bf6
KelseyEng <74799106+KelseyEng@users.noreply.github.com>
2022-07-22 08:29:22 -0400

    Format Update

--- Dice_2022-07-15/dice.py
+++ Dice_2022-07-15/dice.py
@@ -14,19 +14,18 @@ from itertools import product
 # Now how many unique ways are there to mark the die?
 
 
-
-## Gameplan
+# Gameplan
 # If we ignored rotations, then this would be relatively straight forward. Each
 # face could have 2 possibilties, so the number of possibilities would be
 # 2^6, or 64. With the extra credit, that grows to 4^6, or 4096. Both are easily
-# solved with brute force. So, the strategy is to weed out rotations. 
+# solved with brute force. So, the strategy is to weed out rotations.
 # This also sets our bounds. For the first section, our answer should be
 # between 1 and 64. For the extra credit, between 1 and 4096.
 
 # 1. We will create a virtual die whose markings will be either -1 or 1. These
-# can change due to rotation. 
-# 2. So, we will work out the different rotations and how they move the faces 
-# and change the markings. 
+# can change due to rotation.
+# 2. So, we will work out the different rotations and how they move the faces
+# and change the markings.
 # 3. Go through all combinations and weed out copies.
 # 4. For extra credit, just add -2 and 2 as possibilities. Rotations are same.
 
@@ -66,13 +65,13 @@ def flipdie(pos, mod):
     return [pos, mod]
 
 
-def die_perms(position,mod):
-    positionlist = np.zeros([24,6], dtype=int)
-    modlist = np.zeros([24,6], dtype=int)
+def die_perms(position, mod):
+    positionlist = np.zeros([24, 6], dtype=int)
+    modlist = np.zeros([24, 6], dtype=int)
     count = 0
     for i in range(6):
-        #Rotate die into correct position (Number on Top)
-        if position[0] != i: 
+        # Rotate die into correct position (Number on Top)
+        if position[0] != i:
             if position[5] == i:
                 [position, mod] = flipdie(position, mod)
                 [position, mod] = flipdie(position, mod)
@@ -80,65 +79,67 @@ def die_perms(position,mod):
                 while position[3] != i:
                     [position, mod] = twistdie(position, mod)
                 [position, mod] = flipdie(position, mod)
-        
+
         # Get 4 unique configurations with Number on Top
         for j in range(4):
-            positionlist[count,] = position
-            modlist[count,] = mod
-            count +=1
+            positionlist[count, ] = position
+            modlist[count, ] = mod
+            count += 1
             [position, mod] = twistdie(position, mod)
-    
+
     return [positionlist, modlist]
 
+
 def possible_combos(a):
-    
-    possible_list = product(a,a,a,a,a,a)
+
+    possible_list = product(a, a, a, a, a, a)
     possible_list = np.fromiter(possible_list, dtype=np.dtype((int, 6)))
     return possible_list
 
 
-def all_die_rotations(current_val,positionlist,modlist):
+def all_die_rotations(current_val, positionlist, modlist):
     all_vals = []
     for i in range(len(positionlist)):
         all_vals.append(current_val[positionlist[i]] * modlist[i])
     all_vals = np.array(all_vals)
     return all_vals
-    
 
-def find_unique(possible_list,positionlist,modlist):
-    combo_history = np.empty([0,6])
+
+def find_unique(possible_list, positionlist, modlist):
+    combo_history = np.empty([0, 6])
     for i in range(len(possible_list)):
         found_match = 0
         current_val = possible_list[i]
-        if np.any(np.all(combo_history == current_val, axis = 1)):
+        if np.any(np.all(combo_history == current_val, axis=1)):
             print('skip')
             continue
         else:
-            all_vals = all_die_rotations(current_val,positionlist,modlist)
+            all_vals = all_die_rotations(current_val, positionlist, modlist)
             for rotated_val in all_vals:
                 #rotated_val = rotated_val.reshape([1,6])
-                if np.any(np.all(rotated_val == combo_history,axis = 1)):
+                if np.any(np.all(rotated_val == combo_history, axis=1)):
                     found_match = 1
                     break
             if found_match == 1:
                 continue
-            else: 
-                current_val = current_val.reshape([1,6])
-                combo_history = np.append(combo_history,current_val,axis=0)
+            else:
+                current_val = current_val.reshape([1, 6])
+                combo_history = np.append(combo_history, current_val, axis=0)
     return combo_history
+
 
 # Regular
 [pos, mod] = initialdie()
-[poslist, modlist] = die_perms(pos,mod)
-possible_list = possible_combos([-1,1])
+[poslist, modlist] = die_perms(pos, mod)
+possible_list = possible_combos([-1, 1])
 
-combo_history = find_unique(possible_list,poslist,modlist)
+combo_history = find_unique(possible_list, poslist, modlist)
 print(combo_history)
 print(len(combo_history))
 
 # Extra Credit
-possible_list = possible_combos([-2,-1,1,2])
+possible_list = possible_combos([-2, -1, 1, 2])
 
-combo_history = find_unique(possible_list,poslist,modlist)
+combo_history = find_unique(possible_list, poslist, modlist)
 print(combo_history)
 print(len(combo_history))
